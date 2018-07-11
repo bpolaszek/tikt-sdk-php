1fa8ccce4b1e5cb327f07173404e2c09b2e8938c
Julien D <julien@unitylab.io>
2018-07-11 13:30:33 +0200

    update(composer-config): set minimum-stability to stable + bump version to 0.1.0

--- src/Tikt/Dating/Client.php
+++ src/Tikt/Dating/Client.php
@@ -5,6 +5,10 @@ namespace Tikt\Dating;
 class Client extends \Tikt\AbstractClient {
   const DEFAULT_ENDPOINT = 'https://api.dating.tikt.net';
 
+  const ACTIONS = [
+    'DescribeWebsite' => [ 'method' => 'GET' ]
+  ];
+
   public function partnerDescribeUser($params = []) {
     $response = $this->executeGetAction('PartnerDescribeUser', $params);
     return $response->getData();
